077e9b3b904ab54800737ba46886560190ac2448
aindrila-choudhuri <choudhuri.aindrila@gmail.com>
2021-01-25 09:43:53 +0530

    Added git ignore and one route

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello Ubuntu from Node"));
 
+app.get("/about", (req, res) => res.send("About page"));
+
 app.listen(8080, () => console.log("app is running on port 8080"));
